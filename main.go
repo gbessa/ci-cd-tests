3b8cca4ee5f16086926f12318ad8701ae3ce02ae
Gustavo Bessa <gbvirtual@gmail.com>
2020-10-20 15:02:35 +0100

    Msg modified

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>CI/CD FullCycle course Modified 02!")
+}
+
+func main() {
+	var port = ":8080"
+	fmt.Printf("Listening at port %s...", port)
+	http.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(port, nil))
+
+}
